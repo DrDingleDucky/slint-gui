33dd213ec3cae802cca9c7d0ac9d3ef8ad56c686
George Davidson <georgedavidson392@gmail.com>
2024-01-15 16:50:38 -0500

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-slint::include_modules!();
+fn main() {
+    MainWindow::new().unwrap().run().unwrap();
+}
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
-
-    let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
-    });
-
-    ui.run()
+slint::slint! {
+    export component MainWindow inherits Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
 }
