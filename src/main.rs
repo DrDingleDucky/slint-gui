041304d90fee9700df65f7ca007e0f7c0648dd59
George Davidson <georgedavidson392@gmail.com>
2024-01-31 14:51:03 -0500

    added back funcinality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 slint::include_modules!();
 
-fn main() {
-    let main_window = MainWindow::new().unwrap();
+fn main() -> Result<(), slint::PlatformError> {
+    let main_window = MainWindow::new()?;
 
-    main_window.run().unwrap();
+    main_window.on_request_modify_expression({
+        let main_window_handle = main_window.as_weak();
+        move || {
+            let main_window = main_window_handle.unwrap();
+            let mut expression = main_window.get_expression().to_string();
+            expression.pop();
+            main_window.set_expression(expression.into());
+        }
+    });
+
+    main_window.run()
 }
