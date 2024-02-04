ed974bb66ee653e5cdf83de729c7e9f035739d46
George Davidson <georgedavidson392@gmail.com>
2024-02-03 22:04:36 -0500

    updated error handling

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,75 @@ fn eval(num1: i32, num2: i32, operator: &str) -> i32 {
     }
 }
 
+fn infix_to_postfix(expression: String) -> Vec<String> {
+    let mut operator_stack: Vec<String> = Vec::new();
+    let mut output_stack: Vec<String> = Vec::new();
+    for token in expression.split_whitespace().map(String::from) {
+        if let Ok(_number) = token.parse::<i32>() {
+            output_stack.push(token);
+        } else if operator_stack.is_empty()
+            || precedence(&token)
+                > precedence(
+                    &operator_stack
+                        .last()
+                        .map(|s: &String| s.as_str())
+                        .unwrap_or_default(),
+                )
+            || token == "("
+        {
+            operator_stack.push(token);
+        } else if token == ")" {
+            while operator_stack
+                .last()
+                .map(|s: &String| s.as_str())
+                .unwrap_or_default()
+                != "("
+            {
+                output_stack.push(operator_stack.pop().unwrap_or_default());
+            }
+            operator_stack.pop();
+        } else {
+            while !operator_stack.is_empty()
+                && precedence(&token)
+                    <= precedence(
+                        &operator_stack
+                            .last()
+                            .map(|s: &String| s.as_str())
+                            .unwrap_or_default(),
+                    )
+            {
+                output_stack.push(operator_stack.pop().unwrap_or_default());
+            }
+            operator_stack.push(token);
+        }
+    }
+
+    while !operator_stack.is_empty() {
+        output_stack.push(operator_stack.pop().unwrap_or_default());
+    }
+    return output_stack;
+}
+
+fn postfix_eval(postfix_stack: Vec<String>) -> i32 {
+    let mut operand_stack: Vec<String> = Vec::new();
+    for token in postfix_stack {
+        if let Ok(_number) = token.parse::<i32>() {
+            operand_stack.push(token);
+        } else {
+            let num2: i32 = operand_stack
+                .pop()
+                .map(|s: String| s.parse().unwrap_or_default())
+                .unwrap_or_default();
+            let num1: i32 = operand_stack
+                .pop()
+                .map(|s: String| s.parse().unwrap_or_default())
+                .unwrap_or_default();
+            operand_stack.push(eval(num1, num2, &token).to_string());
+        }
+    }
+    return operand_stack.get(0).unwrap_or(&String::from("0")).parse::<i32>().unwrap_or_default();
+}
+
 fn main() -> Result<(), slint::PlatformError> {
     let main_window: MainWindow = MainWindow::new()?;
 
@@ -37,43 +106,10 @@ fn main() -> Result<(), slint::PlatformError> {
             let main_window: MainWindow = main_window_handle.unwrap();
             let expression: String = main_window.get_expression().to_string();
 
-            let mut operator_stack: Vec<String> = Vec::new();
-            let mut output_stack: Vec<String> = Vec::new();
+            let postfix_stack: Vec<String> = infix_to_postfix(expression);
+            let ans: i32 = postfix_eval(postfix_stack);        
 
-            for token in expression.split_whitespace().map(String::from) {
-                if let Ok(_number) = token.parse::<i32>() {
-                    output_stack.push(token);
-                } else if operator_stack.is_empty() || precedence(&token) > precedence(&operator_stack.last().unwrap().to_string()) || token == "(" {
-                    operator_stack.push(token);
-                } else if token == ")" {
-                    while operator_stack.last().unwrap().to_string() != "(" {
-                        output_stack.push(operator_stack.pop().unwrap().to_string());
-                    }
-                    operator_stack.pop();
-                } else {
-                    while !operator_stack.is_empty() && precedence(&token) <= precedence(&operator_stack.last().unwrap().to_string()) {
-                        output_stack.push(operator_stack.pop().unwrap().to_string());
-                    }
-                    operator_stack.push(token);
-                }
-            }
-
-            while !operator_stack.is_empty() {
-                output_stack.push(operator_stack.pop().unwrap().to_string());
-            }
-
-            let mut operand_stack: Vec<String> = operator_stack;
-
-            for token in output_stack {
-                if let Ok(_number) = token.parse::<i32>() {
-                    operand_stack.push(token);
-                } else {
-                    let num2: i32 = operand_stack.pop().unwrap().to_string().parse::<i32>().unwrap();
-                    let num1: i32 = operand_stack.pop().unwrap().to_string().parse::<i32>().unwrap();
-                    operand_stack.push(eval(num1, num2, &token).to_string());
-                }
-            }
-            main_window.set_expression(operand_stack[0].clone().into());
+            main_window.set_expression(ans.to_string().into());
         }
     });
 
